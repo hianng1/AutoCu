9cf503ef072beb3d3ae31775bb676ea5a1c25654
PHLoi313 <loiphps36836@fpt.edu.vn>
2025-05-04 17:13:42 +0700

    Luu thong tin gui mail va cap nhat chuc nang chat truc tuyen

---
+++ src/main/java/poly/edu/Model/Message.java
@@ -0,0 +1,21 @@
+package poly.edu.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    private String sender;
+    private String content;
+    private MessageType type;
+    private String role;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+} 
---
+++ src/main/java/poly/edu/Repository/TicketHoTroRepository.java
@@ -0,0 +1,9 @@
+package poly.edu.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import poly.edu.Model.TicketHoTro;
+
+@Repository
+public interface TicketHoTroRepository extends JpaRepository<TicketHoTro, Long> {
+} 
